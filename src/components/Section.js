d90532a3de914a4380ba90daacc7742bf3c59614
Astrid Lujo <84393461+astridlujo@users.noreply.github.com>
2021-06-07 08:36:36 -0500

    Aligned the buttoms and the animated arrow down

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,7 +6,15 @@ const Section = () => {
         <Container >
             <ItemText>
                 <h1>Model S</h1>
+                <p>Order Online for <u>Toucheless Delivery</u></p>
             </ItemText>
+
+            <ButtonGroup>
+                <LeftButton>Custom Order</LeftButton>
+                <RightButton>Existing Inventory</RightButton>
+            </ButtonGroup>
+
+            <DownArrow src="/images/down-arrow.svg" />
         </Container>
     )
 }
@@ -20,7 +28,45 @@ const Container = styled.div`
     background-repeat: no-repeat;
     width: 100%;
     height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+    flex-grow: 1;
+`
 
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    border-radius: 100px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    font-size: 15px;
+    opacity: 0.85;
+    margin: 8px;
+    cursor: pointer;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    color: black;
+    opacity: 0.85;
+`
+
+const DownArrow = styled.img`
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+    margin-bottom: 40px;
 `
